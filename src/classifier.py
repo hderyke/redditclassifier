cebd2db43e2ef63595c27966b187577b8a33d10b
hderyke <111810343+hderyke@users.noreply.github.com>
2024-05-29 02:26:37 -0400

    Refactored, added some error handling,
    cleaned up debugging statements

--- src/classifier.py
+++ src/classifier.py
@@ -1,22 +1,73 @@
+import datetime
+from datetime import datetime, timedelta
+import sys
+
+import praw
 from transformers import BertForSequenceClassification, BertTokenizer
 import torch
 
+#classifier. Usage: python classifier.py {subreddit name} {n/o days to scan}
+
+def fetch_posts(subreddit_name,days): #get posts given subreddit name and no days
+    print("Scanning the "+subreddit_name+" subreddit from the last "+days+" days")
+
+    reddit = praw.Reddit(
+        client_id='6QfzZm6XRYDKE7Kj_An9EA',
+        client_secret='jAkuGVm4uL0XDM6LKrEoAvz2oqTnKA',
+        user_agent='my_reddit_scraper/0.1 by hderyke59'
+    )
+
+    all_posts = []
+
+    # Calculate the timestamp for the start of the desired time period
+    end_time = datetime.now()
+    days = int(days)
+    start_time = end_time - timedelta(days=days)
+
+    # Access the subreddit
+    subreddit = reddit.subreddit(subreddit_name)
+
+    # Fetch posts using the 'search' method with the 'timestamp' filter
+    for post in subreddit.new(limit=1000):
+        post_created_time = datetime.fromtimestamp(post.created_utc)
+        if start_time <= post_created_time <= end_time:
+            combined_text = f"{post.title} {post.selftext}"
+            all_posts.append(combined_text)
+
+    return all_posts
+
+
+num_args = len(sys.argv)
+if num_args == 3:
+    prompts = fetch_posts(sys.argv[1],sys.argv[-1])
+else:
+    print("Usage: reddit_scan {subreddit name} {n/o days to scan}")
+    exit(0)
+
 # Load the trained model (or untrained)
-
 model = BertForSequenceClassification.from_pretrained('../saved_model')
-
 # Load the tokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
+
 # Preprocess the new data
-text = "my house was built in the 90’s and it still has the original A/C unit but it finally gave out last week. It is a dual system and the heat still works but it is time for a full replacement. Looking for a middle of the road system that will not break the bank and for a team that will get the job done right!"
+# test prompts
+for text in prompts:
+    inputs = tokenizer(text, truncation=True, padding=True, max_length=128, return_tensors="pt")
 
-inputs = tokenizer(text, truncation=True, padding=True, max_length=128, return_tensors="pt")
+    # Make predictions
+    with torch.no_grad():
+        outputs = model(**inputs)
 
-# Make predictions
-with torch.no_grad():
-    outputs = model(**inputs)
+    # Post-process the predictions
+    predictions = torch.sigmoid(outputs.logits)
+    #print(predictions)
+    pred_0_first = predictions[0][0].item()
+    pred_last_second = predictions[-1][1].item()
 
-# Post-process the predictions
-predictions = torch.sigmoid(outputs.logits)
-print(predictions)
+    # safely compare these values
+    if pred_0_first > pred_last_second:
+        continue
+    else:
+        print("Target post")
+        print(text)
--- src/scraper.py
+++ src/scraper.py
@@ -1,30 +1,43 @@
 import csv
+import os
+import time
+
 import praw
 
 #scrape posts from a reddit page and manually label them, add data to csv file
 
 # Define the subreddit and number of posts to fetch
-subreddit_names = ['nyc','houston']
-keywords = ['sports','rec sports','make friends','meet people','club sports','things to do']
-number_of_posts = 3000
+subreddit_names = ['Pensacola']
+keywords = ['rec sports','rec league','intramural']
+number_of_posts = 5000
 
-def fetch_posts(reddit, subreddits, limit, keywords=None,):
+def fetch_posts(reddit, subreddits, limit=10000, keywords=None):
     all_posts = []
 
     if keywords:
         for subreddit_name in subreddits:
             subreddit = reddit.subreddit(subreddit_name)
-            for post in subreddit.new(limit=limit):
-                if any(keyword.lower() in post.title.lower() or keyword.lower() in post.selftext.lower() for keyword in
-                       keywords):
-                    all_posts.append(post)
+            print(f"Fetching new posts from subreddit: {subreddit_name}")
+            try:
+                for post in subreddit.new(limit=limit):
+                    # Check if any keyword is in the post title or selftext
+                    if any(keyword.lower() in post.title.lower() or keyword.lower() in post.selftext.lower() for keyword
+                           in keywords):
+                        all_posts.append(post)
+                        if len(all_posts) >= limit:
+                            break
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            time.sleep(2)  # Delay to avoid hitting rate limits
     else:
         for subreddit_name in subreddits:
             subreddit = reddit.subreddit(subreddit_name)
-            for post in subreddit.new(limit=limit):
+            for post in subreddit.new(limit=number_of_posts):
+                print(f"Found post: {post.title}")
                 all_posts.append(post)
 
     return all_posts
+
 
 
 # Initialize the Reddit client
@@ -37,16 +50,21 @@ reddit = praw.Reddit(
 # Fetch posts from the subreddit
 posts = fetch_posts(reddit, subreddit_names, number_of_posts,keywords)
 
+csv_exist = 1 #check if first entry to posts csv
+if os.path.exists('../data/posts.csv'):
+    csv_exist = 0
+
 # Open the CSV file for writing
 with open('../data/posts.csv', mode='a', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     # Write the header
-    writer.writerow(['Title', 'Body', 'URL', 'Label'])
+    if csv_exist == 0:
+        writer.writerow(['Title', 'Body', 'URL', 'Label'])
 
     # Write the data for each post
     for post in posts:
-        # Clear the terminal screen (cross-platform)
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        # Clear the terminal screen (cross-platform solution)
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
         # Display the post title and body for labeling
         print(f"TITLE: {post.title}\n")
@@ -54,8 +72,10 @@ with open('../data/posts.csv', mode='a', newline='', encoding='utf-8') as file:
 
         # Prompt for labeler to label each post
         label = -1
-        while label != '1' and label != '0':
-            label = input("Enter label (1 for target intent, 0 for other): ")
+        while label != '1' and label != '0' and label != 's':
+            label = input("Enter label (1 for target intent, 0 for other) or 's' to skip: ")
+        if label == 's':
+            continue
 
 
         # Replace commas in text to prevent CSV format issues
